0550dd5c66e0359aa6514ad822921e9d02625a4c
Derek Razo <derekrazo@Dereks-MacBook-Pro-2.local>
2013-08-03 17:59:43 -0700

    adding tray

---
+++ src/Tray.java
@@ -0,0 +1,32 @@
+/*
+ * A tray is a representation of a board state. 
+ * Trays will be stored inside a graph.
+ * Tray is a vertex in the graph of tray states.
+ * 
+ * Knows:
+ * 
+ * 1. Position of blocks
+ * 2. position of spaces
+ * 3. Weight which represents its closeness to another tray
+ * 
+ * Does:
+ * 
+ * 
+ * 
+ * 
+ * */
+
+
+public class Tray {
+
+	public Tray(String[] config)
+	{
+		
+	}
+	
+	public Tray(int[][] input_tray)
+	{
+		
+	}
+	
+}
