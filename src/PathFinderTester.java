6b10b3f697df23aa4c25891ddbb4588e2535f416
okinm117 <niko@mcn.org>
2013-08-04 19:45:05 -0700

    Made Working test for pathfinder

--- src/PathFinderTester.java
+++ src/PathFinderTester.java
@@ -1,6 +1,31 @@
+
 public class PathFinderTester {
-  public void main(String[] args)
-	{	
+	public static void main(String[] args)
+	{
+		int [][] startBoard =
+			{{-1,2,2,2},
+			{-1,0,-1,-1},
+			{-1,-1,-1,-1},
+			{-1,-1,1,-1}};
+		
+		Block [] startBlocks = {new Block(1,1,1,1),new Block(3,2,3,2),new Block(0,1,0,3)};
+		
+		int [][] endBoard = 
+			{{1,-1,-1,-1},
+			{0,-1,-1,-1},
+			{-1,-1,-1,-1},
+			{2,2,2,-1}};
+
+		Block [] endBlocks = {new Block(1,0,1,0),new Block(0,0,0,0),new Block(3,0,3,2)};
+
+		Tray trayStart = new Tray(startBoard, startBlocks);
+		Tray trayEnd = new Tray(endBoard, endBlocks);
+		
+		PathFinder testPath = new PathFinder(trayStart, trayEnd);
+		
+		String [] solution = testPath.solution();
+
+		/*
 		int [][] startBoard =
 			{{-1,-1,-1,-1},
 			{-1, 0, -1, -1},
@@ -23,5 +48,6 @@ public class PathFinderTester {
 		PathFinder testPath = new PathFinder(trayStart, trayEnd);
 		
 		String [] solution = testPath.solution();
+		*/
 	}
 }
