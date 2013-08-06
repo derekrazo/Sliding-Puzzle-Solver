d42f35e11cc5d26d0dca668338d812385bcbd062
irongavel <tohmajudge@berkeley.edu>
2013-08-05 22:40:20 -0700

    Add Comments

--- src/Block.java
+++ src/Block.java
@@ -1,5 +1,7 @@
-
 public class Block {
+	//holds the index of:
+	//leftmost column, rightmost colum, top row, bottom row
+	//also contains the length (row) and the Height(column)
 
 	public int leftCol;
 	public int topRow;
@@ -10,6 +12,7 @@ public class Block {
 	
 	public Block(int x1,int y1, int x2, int y2)
 	{
+		//takes in (x,y) of top left corner, then (x,y) of bottom right corner
 		leftCol = x1;
 		topRow = y1;
 		rightCol = x2;
@@ -20,6 +23,7 @@ public class Block {
 	
 	public Block(Block previous)
 	{
+		//takes in a block, makes a clone of it
 		leftCol = previous.leftCol;
 		topRow = previous.topRow;
 		rightCol = previous.rightCol;
@@ -30,6 +34,7 @@ public class Block {
 	
 	public boolean equals(Block b)
 	{
+		//compares two blocks, returns true if all variables are equal
 		if ((leftCol == b.leftCol) && 
 			(topRow == b.topRow) &&
 			(rightCol == b.rightCol) &&
