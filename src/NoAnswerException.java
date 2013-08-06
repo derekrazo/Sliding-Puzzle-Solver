c6354fb28e54c613f2142ad91327966389b15ced
irongavel <tohmajudge@berkeley.edu>
2013-08-05 22:45:55 -0700

    Add Comments

--- src/NoAnswerException.java
+++ src/NoAnswerException.java
@@ -1,6 +1,7 @@
 public class NoAnswerException extends Exception {
 
   public NoAnswerException (String errMsg) {
+  	//exception for signalling that there are no possible answers
 		super (errMsg);
 	}
 }
